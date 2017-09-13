638191f4109a74cf3e919b946ece2c45a2ceed80
Erik Petersson <erik.petersson@husqvarnagroup.com>
2017-09-13 10:52:40 +0200

    added cell class

--- src/main/java/gof/Cell.java
+++ src/main/java/gof/Cell.java
@@ -1,5 +1,42 @@
 package gof;
 
+import java.util.Random;
+
 public class Cell {
+
+    int x;
+    int y;
+    boolean isAlive;
+
+    public Cell(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+        final Random rand = new Random();
+        setAlive(rand.nextBoolean());
+    }
+
+    public boolean getAlive() {
+        return isAlive;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(final int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(final int y) {
+        this.y = y;
+    }
+
+    public void setAlive(final boolean isAlive) {
+        this.isAlive = isAlive;
+    }
 
 }
