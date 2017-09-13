a14f7c0d5ba4c63e1b368ec463d61cba4c78dd21
Erik Petersson <erik.petersson@husqvarnagroup.com>
2017-09-13 10:45:05 +0200

    removed all

--- src/main/java/gof/Gof.java
+++ src/main/java/gof/Gof.java
@@ -1,34 +1,8 @@
 package gof;
 
-import com.google.common.collect.Table.Cell;
-
 public class Gof {
 
     public static void main(final String[] args) {
-
-        final int ROWS = 500;
-        final int COLUMS = 500;
-
-        StdDraw.setCanvasSize(COLUMS, ROWS);
-        StdDraw.setYscale(0, ROWS);
-        StdDraw.setXscale(0, COLUMS);
-        StdDraw.setPenRadius(0);
-        StdDraw.setPenColor(StdDraw.BLACK);
-
-        final Cell[][] cells = new Cell[ROWS][COLUMS];
-
-        for (int row = 0; row < ROWS; row++) {
-            for (int col = 0; col < COLUMS; col++) {
-                cells[row][col] = new Cell(row, col);
-            }
-        }
-
-        for (int row = 0; row < ROWS; row++) {
-            for (int col = 0; col < COLUMS; col++) {
-                if (cells[row][col].isAlive) {
-                    StdDraw.point(row, col);
-                }
-            }
-        }
+    }
 
 }
